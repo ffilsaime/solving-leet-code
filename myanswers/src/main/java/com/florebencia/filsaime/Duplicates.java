e227a4aa5311c03f3faa18e395fc226f26799f61
Florebencia <florebencia.filsaime1998@gmail.com>
2026-03-22 12:25:39 -0400

    Solved ContainsDuplicates LeetCode Problem

---
+++ myanswers/src/main/java/com/florebencia/filsaime/Duplicates.java
@@ -0,0 +1,20 @@
+package com.florebencia.filsaime;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leet code problem: https://leetcode.com/problems/contains-duplicate/submissions/1955915126/
+public class Duplicates {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
