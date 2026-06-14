db931f2ab082532c6bba9e0e7552421a1b2397a3
Florebencia <florebencia.filsaime1998@gmail.com>
2026-06-14 19:49:00 -0400

    Solved Problem Number 88 in Leetcode with a heap

--- myanswers/src/main/java/com/florebencia/filsaime/HeapProblems.java
+++ myanswers/src/main/java/com/florebencia/filsaime/HeapProblems.java
@@ -1,6 +1,7 @@
 package com.florebencia.filsaime;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class HeapProblems {
@@ -39,6 +40,66 @@ public class HeapProblems {
             even.add(value);
         } else {
             odd.add(value);
+        }
+    }
+    //end of leetcode problem
+
+    //for leetcode problem: https://leetcode.com/problems/merge-sorted-array/description/
+    class ArrayComparator implements Comparator<int[]> {
+        public int compare(int[] arr, int[] arr2){
+            return arr[0] - arr2[0];
+        }
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0){
+            return;
+        }
+
+        if (m == 0){
+            //set up all of the values of nums2 into nums1
+            for (int i = 0; i < n; i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+
+        PriorityQueue<int[]> tracker = new PriorityQueue<>(new ArrayComparator());
+        int index = 0;
+        // first index is the val; second index is the array #; last index is the index in the array itself
+        int[] start = {nums1[0], 1, 0};
+        int[] start2 = {nums2[0], 2, 0};
+        int[] sorted = new int[nums1.length];
+        tracker.add(start);
+        tracker.add(start2);
+
+        //instead of replacing the values in nums1 put in in another array and change nums1's values to that
+        while (!tracker.isEmpty()){
+            int[] lowest = tracker.poll();
+            sorted[index] = lowest[0];
+            int[] newArr = new int[3];
+            int newIndex = lowest[2] + 1;
+            if (lowest[1] == 1){
+                // it comes from the first array
+                if (newIndex < m){
+                    newArr[0] = nums1[newIndex];
+                    newArr[1] = 1;
+                    newArr[2] = newIndex;
+                    tracker.add(newArr);
+                }
+            } else {
+                if (newIndex < n){
+                    newArr[0] = nums2[newIndex];
+                    newArr[1] = 2;
+                    newArr[2] = newIndex;
+                    tracker.add(newArr);
+                }
+            }
+            index++;
+        }
+
+        for (int i = 0; i < nums1.length; i++){
+            nums1[i] = sorted[i];
         }
     }
     //end of leetcode problem
